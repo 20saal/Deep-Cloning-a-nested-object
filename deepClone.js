4d159c9087c4f421128ff6510e58be5be0d8bd7d
20saal <110736181+20saal@users.noreply.github.com>
2022-08-29 16:30:27 +0530

    Clone both nested object and array
    This block of code uses two separate custom function  'CloneObj' and 'CloneArray' to deep clone both nested objects and arrays.

--- deepClone.js
+++ deepClone.js
@@ -1,15 +1,13 @@
-// Deep copy of Nested object using constructor-function
-
+// Deep copying Nested object and Array using two different constructor function
 let obj = {
-    level01:'level01',
     level1: {
-        level2: {
-            level03:'level03',
-            level3:{
-                level4:'level4',
-            },
-        },
+        level2:[{
+            level3:['level3'], level03:'level03'},
+            'level4'
+        ],
+        level02:'level02',
     },
+    level01:'level01',
 };
 
 function CloneObj(obj){
@@ -33,23 +31,55 @@ function CloneObj(obj){
         }
         for(let key in obj)
         {
-            if(typeof obj[key] === 'object')
+            if(typeof obj[key] === 'object'  && !Array.isArray(obj[key]))
             {
                 this[key] = new CloneObj(obj[key] ); 
+            }
+            else if(Array.isArray(obj[key]))
+            {
+                this[key] = CloneArray(obj[key]);
             }
             else{
                 this[key] = obj[key];
             }
         }
-        
     }
 }
 
+//function copy array and return copied array
+function CloneArray(array)
+{
+    let copiedArray = [];
+        for(let elm of array)
+    {
+        if(typeof elm == 'object' && !Array.isArray(elm))
+        {
+            copiedArray.push(new CloneObj(elm));
+        }
+        else if(Array.isArray(elm))
+        {
+            copiedArray.push(CloneArray(elm));
+        }
+        else{
+            copiedArray.push(elm);
+        }
+    }
+    return copiedArray;
+}
+    
+//Nested object
 let copiedObj = new CloneObj(obj);
-console.log(typeof copiedObj.level1.level2.level3.level4); //'level4'
-console.log(copiedObj.level1.level2 == obj.level1.level2); //false
-console.log(copiedObj.level1.level2.level03); //'level03'
+console.log(copiedObj.level1.level2[0].level3); //['level3']
+console.log(copiedObj.level1.level2[0].level03); //level03
+console.log(obj.level1.level2 == copiedObj.level1.level2); //false means array at level2 is not copied by refrence'
 
 
 let copiedAnotherObj =new CloneObj('hello');
 console.log(copiedAnotherObj);//{undefined:undefined}
+
+//Nested array
+let nestedArray = [{level1:['level1']}, 'level2', [{level3:'level3', level4:'level4'}]];
+
+let copiedArray = CloneArray(nestedArray);
+console.log(copiedArray[2][0].level3); //'level3'
+console.log(nestedArray[2][0] == copiedArray[2][0]);//false b/c not copied by refrence
